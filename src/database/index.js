79301b8abed2ab72aed2c773ccabe7ec47ce5b64
akdukey <akdukey@gmail.com>
2025-05-26 21:21:49 +0530

    F

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function connectToDB(){
     try {
         await mongoose.connect(
-            "mongodb+srv://admin:fuckduldul@cluster0.60bar.mongodb.net/portfolio"
+            "mongodb+srv://admin:fuckduldul@cluster0.jgit9k1.mongodb.net/hello"
         );
         console.log('Database connected successfully');
     } catch (e) {
