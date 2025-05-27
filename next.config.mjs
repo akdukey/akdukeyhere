55c1d16566d7287bbdd92cda781b013875cfccf3
akdukey <akdukey@gmail.com>
2025-05-27 11:41:41 +0530

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'], // ← replace with your image's domain, e.g., 'cdn.myimages.net'
+    domains: ['images.unsplash.com','cdn.pixabay.com'], // ← replace with your image's domain, e.g., 'cdn.myimages.net'
   },
 };
 
